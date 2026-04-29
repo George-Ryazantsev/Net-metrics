c42f05eb131dc490537b116b3a618aee577ee2dc
Георгий Рязанцев <g.ryazancev@WS-288>
2026-04-29 11:06:21 +0300

    develop Инициализирован новый проект

---
+++ Net metrics/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net_metrics.Controllers
+{
+    [ApiController]
+    [Route("api/events")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("ping")]
+        public IActionResult Ping()
+        {
+            _logger.LogInformation("Ping event received at {Time}", DateTime.UtcNow);
+            return Ok();
+        }
+    }
+}
--- Net metrics/Program.cs
+++ Net metrics/Program.cs
@@ -1,8 +1,18 @@
+using Net_metrics.Services;
+using ServiceCloud.Extensions.Logging.File;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+//builder.Logging.AddJsonFileLogger(options=> options.FilePath = "C:\\Users\\g.ryazancev\\Desktop\\Metrics\\metrics.txt");
+builder.Logging.AddJsonFileLogger(options => options.FilePath = "/home/dev1/metrics/Output/metrics.txt");
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
+builder.Services.AddScoped<IMetricsService, MetricsService>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -11,13 +21,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    Console.WriteLine("Application started!!!");
+});
 
 app.Run();
